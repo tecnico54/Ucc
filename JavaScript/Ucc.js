da76f0fc79a79a7d5a1d69d9756b3f30cba9fd13
tecnico54 <115370228+tecnico54@users.noreply.github.com>
2025-01-04 16:16:24 -0500

    Update Ucc.js

--- JavaScript/Ucc.js
+++ JavaScript/Ucc.js
@@ -4,7 +4,6 @@ let fecha = new Date();
 let mesAnyo = ['Enero','Febrero', 'Marzo','abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
 let diaSemana = ['Domingo','Lunes', 'Martes','Miércoles','Jueves','Viernes','Sábado'];
 mostrarFecha.innerHTML = `${diaSemana[fecha.getDay()]}, ${fecha.getDate()} de ${mesAnyo[fecha.getMonth()]} de ${fecha.getFullYear()}`;
-
 setInterval(()=>{
   let hora = new Date();
   mostrarReloj.innerHTML = hora.toLocaleTimeString();
