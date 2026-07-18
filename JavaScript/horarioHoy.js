dc6be91b5c8edadd5917b466b685e0dbd51ef002
tecnico54 <115370228+tecnico54@users.noreply.github.com>
2026-07-18 17:12:31 -0500

    Update horarioHoy.js

--- JavaScript/horarioHoy.js
+++ JavaScript/horarioHoy.js
@@ -14,11 +14,35 @@ const clasesHoy = materias.filter(materia => materia.dia === hoy);
 if(clasesHoy.length === 0){
 	contenedor.innerHTML = `
 		<h3>Hoy es ${hoy}</h3>
-		<p>No tienes clases hoy 😄</p>
-	`;
+		<p class="mensaje">
+		🎉 Hoy no tienes clases.
+		</p>
+		<p>Aprovecha para descansar o estudiar 😄</p>	
+`;
 	return;
 }
-let html = `<h3>Hoy es ${hoy}</h3>`;
+//Contar cuántas clases hay hoy
+const cantidad = clasesHoy.length;
+let html = `
+	<h3>Hoy es ${hoy}</h3>
+`;
+//Mensaje según la cantidad de clases
+if(cantidad == 1){
+	html += `
+	<p class="mensaje">
+	📚 Hoy tienes <b>1 clase</b>.
+	</p>
+	<p><b>¡Muchos éxitos en la universidad!</b> 💪</p>
+	`;
+}else{
+	html += `
+	<p class="mensaje">
+	📚 Hoy tienes <b>${cantidad} clases</b>.
+	</p>
+	<p><b>¡Muchos éxitos en la universidad!</b> 💪</p>
+	`;
+}
+//Mostrar las materias
 clasesHoy.forEach(materia=>{
 	html += `
 	<div class="materia">
