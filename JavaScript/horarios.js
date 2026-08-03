c2507694687aad35a603224aadd3881fc7eff2e9
tecnico54 <115370228+tecnico54@users.noreply.github.com>
2026-08-03 00:26:36 -0500

    Update horarios.js

--- JavaScript/horarios.js
+++ JavaScript/horarios.js
@@ -2,41 +2,41 @@ const materias = [
 {
     nombre: "Prueba y Mantenimiento",
     dia: "Lunes",
-    semestre: 7,
+    semestre: "7",
     hora: "8:00 - 12:00",
     bloque: "1",
-    salon: "Bloque 1-605"
+    salon: "605"
 },
 {
     nombre: "Calculo Integrar",
     dia: "Martes",
-    semestre: 2,
+    semestre: "2",
     hora: "14:00 - 17:00",
     bloque: "1",
-    salon: "Bloque 1-605"
+    salon: "605"
 },
 {
     nombre: "Seguridad Informatica",
     dia: "Martes",
-    semestre: 8,
+    semestre: "8",
     hora: "18:00 - 21:00",
     bloque: "1",
-    salon: "Bloque 1-605"
+    salon: "605"
 },
 {
     nombre: "Gestiones de Proyectos",
     dia: "Miércoles",
-    semestre: 8,
+    semestre: "8",
     hora: "16:00 - 18:00",
     bloque: "1",
-    salon: "Bloque 1-705"
+    salon: "705"
 },
 {
     nombre: "Metodologia de Software Colaborativo",
     dia: "Sábado",
-    semestre: 7,
+    semestre: "7",
     hora: "6:00 - 8:00",
     bloque: "1",
-    salon: "Bloque 1-605"
+    salon: "605"
 }
 ];
